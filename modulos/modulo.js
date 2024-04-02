3e58fb1d82f06b31531372d9b4fe286e5bddb432
Luciano Segundo <103046717+LucianoSegundo@users.noreply.github.com>
2024-04-02 11:45:21 -0300

    Delete modulos directory

--- modulos/modulo.js
+++
@@ -1,6 +0,0 @@
-import trocarTela from "./trocarTela.js";
-import login from "./login.js";
-import mapalealet from "./principal/mapalealet.js";
-import botoesTopo from "./principal/botoesTopo.js";
-
-
